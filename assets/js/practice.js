e745ee808e12df52b923f01d6ef7f4959e53d525
courtneyhook <118090383+courtneyhook@users.noreply.github.com>
2024-02-02 23:36:47 -0600

    added practice html

---
+++ assets/js/practice.js
@@ -0,0 +1,13 @@
+// user selects operation and time
+
+//user presses start
+
+//user gives input
+
+//computer checks for correct or incorrect
+
+//if correct correct score goes up
+
+//if incorrect incorrect score goes up
+
+//at the end of the time, the scores are recapped
